900ab17b4607e20957df608a2606b0250c983647
alexconrey <alexconrey@users.noreply.github.com>
2026-07-25 05:57:31 +0000

    deploy: eee69c25edbc00bf18169a521e4e7c93a1ca05a7

--- latest/searcher-09f2665d.js
+++ latest/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-414a075b.js',
+                path_to_root + 'searchindex-9e9ab823.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
